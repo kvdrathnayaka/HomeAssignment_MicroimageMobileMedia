4dc8f289af5f695c2b1fe1e57a6e678c8597e513
Kavindu Rathnayaka <KavinduReaper@users.noreply.github.com>
2021-09-09 17:11:56 +0530

    Build Bash Script to run java Application

---
+++ bashScript.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+javac factoryPattern.java
+java factoryPattern
+read -n 1 -s -r -p "Press Enter to continue..."
---
+++ factoryPattern.java
@@ -0,0 +1,9 @@
+/**
+ * factoryPattern
+ */
+public class factoryPattern {
+
+    public static void main(String[] args) {
+        System.out.println("Running Application...");
+    }
+}
